d1eede806b8f8e0914a8c6c9190b871d22ee330b
RaiderAerospaceSociety <ras@ttu.edu>
2025-06-25 22:33:51 -0500

    Configured the PlatformIDE project to use FeatherS3 Defaults

--- code/airbrake-primary-controller/src/main.cpp
+++ code/airbrake-primary-controller/src/main.cpp
@@ -1,18 +1,40 @@
-#include <Arduino.h>
+#include <UMS3.h>
 
-// put function declarations here:
-int myFunction(int, int);
+UMS3 ums3;
+
+uint32_t led_cycle_count = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  // Initialize all board peripherals, call this first
+  ums3.begin();
+
+  // Brightness is 0-255. We set it to 1/3 brightness here
+  ums3.setPixelBrightness(255 / 3);
+
+  // Enable the power to the RGB LED.
+  // Off by default so it doesn't use current when the LED is not required.
+  ums3.setPixelPower(true);
 }
+
+int color = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // colorWheel cycles red, orange, ..., back to red at 256
+  ums3.setPixelColor(UMS3::colorWheel(color));
+  color++;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // On the feathers3, toggle the LED twice per cycle
+#ifdef ARDUINO_FEATHERS3
+  if (color % 128 == 0) {
+    ums3.toggleBlueLED();
+  }
+#endif
+
+  if (color % 255 == 0) {
+    Serial.printf("LED cycle #%d\n", ++led_cycle_count);
+  }
+
+  delay(15);
 }
